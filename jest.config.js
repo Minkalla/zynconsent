09f8edd8a6154f5047ad3a9a37787a1c92e7b116
ronakminkalla <ronakcp@gmail.com>
2025-06-16 00:41:47 -0400

    Create jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+    module.exports = {
+      preset: 'ts-jest',
+      testEnvironment: 'node',
+      roots: ['<rootDir>/src', '<rootDir>/tests'],
+      transform: {
+        '^.+\\.ts?$': 'ts-jest',
+      },
+      moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+      testMatch: ['<rootDir>/tests/**/*.test.ts'],
+    };
+    
